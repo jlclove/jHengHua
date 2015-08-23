6ad768b9b83ba089eeb02de244dbf67069398558
jlclove <jlclove@126.com>
2015-08-23 23:18:48 +0800

    1111

--- src/main/java/com/goodlaike/henghua/controller/IndexController.java
+++ src/main/java/com/goodlaike/henghua/controller/IndexController.java
@@ -1,10 +1,7 @@
 package com.goodlaike.henghua.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.goodlaike.business.news.service.NewsService;
 
 /**
  * 
@@ -14,12 +11,8 @@ import com.goodlaike.business.news.service.NewsService;
 @RequestMapping("/")
 public class IndexController {
 
-    @Autowired
-    private NewsService newsService;
-    
-	@RequestMapping("/")
-	protected String index() {
-	    newsService.helloworld();
-		return "index";
-	}
+    @RequestMapping("/")
+    protected String index(){
+        return "index";
+    }
 }
