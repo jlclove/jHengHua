b28f2c4da10f5c5de4d503c33c21c2ac839e4eec
jlclove <jlclove@126.com>
2015-08-25 00:15:07 +0800

    1、代码优化

--- src/main/java/com/goodlaike/henghua/controller/IndexController.java
+++ src/main/java/com/goodlaike/henghua/controller/IndexController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class IndexController {
-
     @RequestMapping("/")
     protected String index(){
         return "index";
