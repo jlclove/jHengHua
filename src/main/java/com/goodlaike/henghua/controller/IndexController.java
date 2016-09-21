cb66bfab1bd9575a67cb1c9debd98c58d6ad3b2c
jlclove <jlclove@126.com>
2016-09-21 18:28:19 +0800

    dddddd

--- src/main/java/com/goodlaike/henghua/controller/IndexController.java
+++ src/main/java/com/goodlaike/henghua/controller/IndexController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.goodlaike.business.core.helper.LanguageHelper;
 import com.goodlaike.business.news.model.News;
@@ -27,174 +28,194 @@ import com.goodlaike.henghua.service.StoreService;
  */
 @Controller
 @RequestMapping("/")
-public class IndexController  extends BaseController{
+public class IndexController extends BaseController {
 
-    @Autowired
-    NewsService newsService;
+	@Autowired
+	NewsService newsService;
 
-    @Autowired
-    private StoreService storeService;
+	@Autowired
+	private StoreService storeService;
 
-    @RequestMapping("/")
-    protected String index() {
-        return "index";
-    }
+	@RequestMapping("/")
+	protected String index() {
+		return "index";
+	}
 
-    @RequestMapping("/service-faq")
-    protected String serviceFaq() {
-        return "service-faq";
-    }
+	@RequestMapping("/service-faq")
+	protected String serviceFaq() {
+		return "service-faq";
+	}
 
-    /**
-     * 了解衬衫
-     * @return
-     */
-    @RequestMapping("/about-our-shirts")
-    protected String aboutOurShirts() {
-        return "about-our-shirts";
-    }
+	/**
+	 * 了解衬衫
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/about-our-shirts")
+	protected String aboutOurShirts() {
+		return "about-our-shirts";
+	}
 
-    /**
-     * 了解西装
-     * @return
-     */
-    @RequestMapping("/about-our-suits")
-    protected String aboutOurSuits() {
-        return "about-our-suits";
-    }
+	/**
+	 * 了解西装
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/about-our-suits")
+	protected String aboutOurSuits() {
+		return "about-our-suits";
+	}
 
-    /**
-     * 媒体报道
-     * @param model
-     * @return
-     */
-    @SuppressWarnings({"rawtypes", "unchecked"})
-    @RequestMapping("/media")
-    protected String media(HttpServletRequest request, Model model) {
-        model.addAttribute(pageName, "media");
-        Pagination pagination = new Pagination();
-        pagination.setPageNo(1);
-        pagination.setPageSize(20);
-        pagination.addFilters(new AbstractSearchFilter[]{SearchFilter.and("type", SearchType.EQUAL, new Object[] {"媒体"})});
-        pagination.addFilters(new OrderFilter[]{OrderFilter.desc("id")});
-        pagination = this.newsService.getPagination(pagination, LanguageHelper.getLocalization(request));
-        model.addAttribute("mediaList", pagination.getList());
-        return "media";
-    }
+	/**
+	 * 媒体报道
+	 * 
+	 * @param model
+	 * @return
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@RequestMapping("/media")
+	protected String media(HttpServletRequest request, Model model) {
+		model.addAttribute(pageName, "media");
+		Pagination pagination = new Pagination();
+		pagination.setPageNo(1);
+		pagination.setPageSize(20);
+		pagination.addFilters(
+				new AbstractSearchFilter[] { SearchFilter.and("type", SearchType.EQUAL, new Object[] { "媒体" }) });
+		pagination.addFilters(new OrderFilter[] { OrderFilter.desc("id") });
+		pagination = this.newsService.getPagination(pagination, LanguageHelper.getLocalization(request));
+		model.addAttribute("mediaList", pagination.getList());
+		return "media";
+	}
 
-    /**
-     * 媒体报道详情
-     * @param id
-     * @param request
-     * @param model
-     * @return
-     */
-    @RequestMapping("/media/{id}")
-    protected String mediaDetail(@PathVariable int id, HttpServletRequest request, Model model){
-        News news = newsService.getNews(id, LanguageHelper.getLocalization(request));
-        model.addAttribute("media", news);
-        return "mediaDetail";
-    }
+	/**
+	 * 媒体报道详情
+	 * 
+	 * @param id
+	 * @param request
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/media/{id}")
+	protected String mediaDetail(@PathVariable int id, HttpServletRequest request, Model model) {
+		News news = newsService.getNews(id, LanguageHelper.getLocalization(request));
+		model.addAttribute("media", news);
+		return "mediaDetail";
+	}
 
-    /**
-     * 新闻
-     * @param model
-     * @return
-     */
-    @SuppressWarnings({"rawtypes", "unchecked"})
-    @RequestMapping("/news")
-    protected String news(HttpServletRequest request, Model model) {
-        model.addAttribute(pageName, "news");
-        Pagination pagination = new Pagination();
-        pagination.setPageNo(1);
-        pagination.setPageSize(5);
-        pagination.addFilters(new AbstractSearchFilter[]{SearchFilter.and("type", SearchType.EQUAL, new Object[] {"新闻"})});
-        pagination.addFilters(new OrderFilter[]{OrderFilter.desc("id")});
-        pagination = this.newsService.getPagination(pagination, LanguageHelper.getLocalization(request));
-        model.addAttribute("newsList", pagination.getList());
-        return "news";
-    }
+	/**
+	 * 新闻
+	 * 
+	 * @param model
+	 * @return
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@RequestMapping("/news")
+	protected String news(HttpServletRequest request, Model model) {
+		model.addAttribute(pageName, "news");
+		Pagination pagination = new Pagination();
+		pagination.setPageNo(1);
+		pagination.setPageSize(5);
+		pagination.addFilters(
+				new AbstractSearchFilter[] { SearchFilter.and("type", SearchType.EQUAL, new Object[] { "新闻" }) });
+		pagination.addFilters(new OrderFilter[] { OrderFilter.desc("id") });
+		pagination = this.newsService.getPagination(pagination, LanguageHelper.getLocalization(request));
+		model.addAttribute("newsList", pagination.getList());
+		return "news";
+	}
 
-    /**
-     * 新闻详情
-     * @param id
-     * @param request
-     * @param model
-     * @return
-     */
-    @RequestMapping("/news/{id}")
-    protected String newsDetail(@PathVariable int id, HttpServletRequest request, Model model){
-        News news = newsService.getNews(id, LanguageHelper.getLocalization(request));
-        model.addAttribute("news", news);
-        return "newsDetail";
-    }
+	/**
+	 * 新闻详情
+	 * 
+	 * @param id
+	 * @param request
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/news/{id}")
+	protected String newsDetail(@PathVariable int id, HttpServletRequest request, Model model) {
+		News news = newsService.getNews(id, LanguageHelper.getLocalization(request));
+		model.addAttribute("news", news);
+		return "newsDetail";
+	}
 
-    /**
-     * 企业信息
-     * @param pageName
-     * @param model
-     * @return
-     */
-    @RequestMapping("/enterprise/{pageName}")
-    protected String enterprise(@PathVariable String pageName, Model model){
-        model.addAttribute(pageName, "pageName");
-        return "enterprise";
-    }
+	/**
+	 * 企业信息
+	 * 
+	 * @param pageName
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/enterprise/{pageName}")
+	protected String enterprise(@PathVariable String pageName, Model model) {
+		model.addAttribute(pageName, "pageName");
+		return "enterprise";
+	}
 
-    /**
-     * 专业服务
-     * @param pageName
-     * @param model
-     * @return
-     */
-    @RequestMapping("/service/{pageName}")
-    protected String service(@PathVariable String pageName, Model model){
-        model.addAttribute(pageName, "pageName");
-        return "service";
-    }
-    
-    /**
-     * 隐私声明
-     * @return
-     * @author jail
-     */
-    @RequestMapping("privacy")
-    protected String privacy(){
-        return "privacy";
-    }
-    
-    /**
-     * 购买条款
-     * @return
-     * @author jail
-     */
-    @RequestMapping("term")
-    protected String term(){
-        return "term";
-    }
+	/**
+	 * 专业服务
+	 * 
+	 * @param pageName
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/service/{pageName}")
+	protected String service(@PathVariable String pageName, Model model) {
+		model.addAttribute(pageName, "pageName");
+		return "service";
+	}
 
-    /**
-     * 全球门店
-     * @return
-     */
-    @RequestMapping("stores")
-    protected String store(@RequestParam int countryId, HttpServletRequest request, Model model){
-        model.addAttribute("storeList", storeService.getStoreList(countryId, LanguageHelper.getLocalization(request)));
-        model.addAttribute("country", StoreCountryStore.getCountry(request, countryId));
-        return "stores";
-    }
+	/**
+	 * 隐私声明
+	 * 
+	 * @return
+	 * @author jail
+	 */
+	@RequestMapping("privacy")
+	protected String privacy() {
+		return "privacy";
+	}
 
-    /**
-     * 门店详情
-     * @return
-     */
-    @RequestMapping("stores/{storeId}")
-    protected String storeDetail(@PathVariable int storeId, HttpServletRequest request, Model model){
-        Store store = storeService.getStore(storeId, LanguageHelper.getLocalization(request));
-        if (store != null) {
-            model.addAttribute("store", storeService.getStore(storeId, LanguageHelper.getLocalization(request)));
-            model.addAttribute("country", StoreCountryStore.getCountry(request, store.getCountryId()));
-        }
-        return "storesDetail";
-    }
+	/**
+	 * 购买条款
+	 * 
+	 * @return
+	 * @author jail
+	 */
+	@RequestMapping("term")
+	protected String term() {
+		return "term";
+	}
+
+	/**
+	 * 全球门店
+	 * 
+	 * @return
+	 */
+	@RequestMapping("stores")
+	protected String store(@RequestParam int countryId, HttpServletRequest request, Model model) {
+		model.addAttribute("storeList", storeService.getStoreList(countryId, LanguageHelper.getLocalization(request)));
+		model.addAttribute("country", StoreCountryStore.getCountry(request, countryId));
+		return "stores";
+	}
+
+	/**
+	 * 门店详情
+	 * 
+	 * @return
+	 */
+	@RequestMapping("stores/{storeId}")
+	protected String storeDetail(@PathVariable int storeId, HttpServletRequest request, Model model) {
+		Store store = storeService.getStore(storeId, LanguageHelper.getLocalization(request));
+		if (store != null) {
+			model.addAttribute("store", storeService.getStore(storeId, LanguageHelper.getLocalization(request)));
+			model.addAttribute("country", StoreCountryStore.getCountry(request, store.getCountryId()));
+		}
+		return "storesDetail";
+	}
+
+	@RequestMapping("health")
+	@ResponseBody
+	protected long health() {
+		return System.currentTimeMillis();
+	}
 }
--- src/main/webapp/resources/javascripts/sampleDetail.js
+++ src/main/webapp/resources/javascripts/sampleDetail.js
@@ -1,10 +1,8 @@
 /**
  * Created by charles on 15/9/10.
  */
-if(hasLogin) {
-    $.get('/henghua/sample/detail/' + detailId + '/quantity', function (data) {
-        $('#stock').text(data.stock);
-        $('#unit').text(data.unit);
-        $('#price').text(data.price);
-    });
-}
+$.get('/henghua/sample/detail/' + detailId + '/quantity', function (data) {
+    $('#stock').text(data.stock);
+    $('#unit').text(data.unit);
+    $('#price').text(data.price);
+});
